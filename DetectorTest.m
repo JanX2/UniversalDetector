3d92871047a1ff9526f705ba777db08c2310ccd9
Jan <github@geheimwerk.de>
2010-04-13 06:15:24 +0200

    Using Foundation.framework now. protypes.h was typedef'ing uint32 => suppressed for 10.5 SDK (better approach?). Improved DetectorTest. Added "Debug Clang" and "Release Clang" build configs. Switched build configs to C99.

--- DetectorTest.m
+++ DetectorTest.m
@@ -1,4 +1,5 @@
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
+
 #import <UniversalDetector/UniversalDetector.h>
 
 int main(int argc,char **argv)
@@ -7,13 +8,18 @@ int main(int argc,char **argv)
 
 	UniversalDetector *detector=[UniversalDetector detector];
 
-	for(int i=1;i<argc;i++)
+	for (int i = 1; i < argc; i++)
 	{
 		NSData *data=[NSData dataWithContentsOfFile:[NSString stringWithUTF8String:argv[i]]];
 		[detector analyzeData:data];
 	}
 
-	printf("%s %d %f\n",[[detector MIMECharset] UTF8String],[detector encoding],[detector confidence]);
+	NSString *str = [NSString stringWithFormat:@"\"%@\" (%s) confidence: %.1f%%\n", 
+					 [NSString localizedNameOfStringEncoding:[detector encoding]], 
+					 [[detector MIMECharset] UTF8String], 
+					 ([detector confidence]*100.0f) 
+					 ];
+	printf("%s", [str cStringUsingEncoding:NSUTF8StringEncoding]);
 
 	[pool release];
 	return 0;
