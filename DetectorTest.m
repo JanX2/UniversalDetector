1873a313167d09e2d6e0c2580f39278c04651a45
Jan <github@geheimwerk.de>
2013-01-30 14:41:04 +0100

    Fixing and improving DetectorTest.

--- DetectorTest.m
+++ DetectorTest.m
@@ -6,25 +6,39 @@ int main(int argc,char **argv)
 {
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 	
-	UniversalDetector *detector = [UniversalDetector new];
-	
 	NSString *str = nil;
+	NSError *error = nil;
 	
 	for (int i = 1; i < argc; i++)
 	{
-		NSData *data = [NSData dataWithContentsOfFile:[NSString stringWithUTF8String:argv[i]]];
+		// You need a new detector for each piece of data you want to examine!
+		UniversalDetector *detector = [UniversalDetector new];
+	
+		NSString *filePath = [NSString stringWithUTF8String:argv[i]];
+		NSString *fileName = [filePath lastPathComponent];
+		
+		NSData *data = [NSData dataWithContentsOfFile:filePath
+											  options:0
+												error:&error];
+		if (data == nil) {
+			str = [NSString stringWithFormat:@"%@\n\t%@", fileName, error];
+			printf("%s", [str UTF8String]);
+			continue;
+		}
+		
 		[detector analyzeData:data];
 		
-		str = [NSString stringWithFormat:@"\"%@\" (%s) confidence: %.1f%%\n", 
+		str = [NSString stringWithFormat:@"%@\n\t\"%@\" (%@) confidence: %.1f%%",
+			   fileName,
 			   [NSString localizedNameOfStringEncoding:[detector encoding]], 
-			   [[detector MIMECharset] UTF8String], 
-			   ([detector confidence]*100.0f) 
+			   [detector MIMECharset], 
+			   ([detector confidence] * 100.0f) 
 			   ];
-		printf("%s", [str cStringUsingEncoding:NSUTF8StringEncoding]);
+		printf("%s\n\n", [str UTF8String]);
 		
-	}
 	
-	[detector release];
+		[detector release];
+	}
 	
 	[pool release];
 	return 0;
