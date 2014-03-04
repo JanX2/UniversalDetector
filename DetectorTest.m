3ba4811ec4d44df7c1c4cad8e073ba5c7b23d4f5
Jan Weiß <jan@geheimwerk.de>
2014-03-04 18:01:13 +0100

    Adding AppKit text encoding guess to DecoderTest and comparing the results.

--- DetectorTest.m
+++ DetectorTest.m
@@ -1,4 +1,5 @@
 #import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
 
 #import <UniversalDetector/UniversalDetector.h>
 
@@ -43,13 +44,58 @@ int main(int argc,char **argv)
 		[detector analyzeData:data];
 		NSString *MIMECharsetName = [detector MIMECharset];
 		NSStringEncoding encoding = [detector encoding];
+		NSStringEncoding appKitEncoding = 0;
 		
-		str = [NSString stringWithFormat:@"%@\n" "\t" "\"%@\" (%@)\n" "\t" "confidence:% 6.1f%%",
+		//if (encoding == NSWindowsCP1252StringEncoding || encoding == NSShiftJISStringEncoding)
+		{
+			NSDictionary *documentAttributes = nil;
+			
+			// UniversalDetector does not differentiate between Windows Latin 1 and Mac Roman
+			// while AppKit has an apparent Mac Roman bias.
+			NSAttributedString *text = [[NSAttributedString alloc] initWithData:data
+																		options:nil
+															 documentAttributes:&documentAttributes
+																		  error:&error];
+			
+			if (text == nil) {
+				NSLog(@"%@", error);
+				continue;
+			}
+			else {
+				[text release];
+				
+				NSNumber *encodingNumber = documentAttributes[NSCharacterEncodingDocumentAttribute];
+				appKitEncoding = [encodingNumber intValue];
+			}
+		}
+		
+		NSString *appKitResultString = nil;
+		if (appKitEncoding != 0) {
+			if (appKitEncoding != encoding) {
+				appKitResultString = [NSString stringWithFormat:@"\"%@\"",
+									  [NSString localizedNameOfStringEncoding:appKitEncoding]
+									  ];
+			}
+			else {
+				appKitResultString = @"(same result)";
+			}
+		}
+		
+		str = [NSString stringWithFormat:
+			   @"%@\n"
+			   "\t" "\"%@\" (%@)\n"
+			   "\t" "confidence:% 6.1f%%"
+			   @"\n"
+			   "\t" "AppKit: %@",
 			   fileName,
 			   (encoding != 0) ? [NSString localizedNameOfStringEncoding:encoding] : @"UNKNOWN",
 			   (MIMECharsetName != nil) ? MIMECharsetName : @"UNKNOWN",
-			   ([detector confidence] * 100.0f) 
+			   ([detector confidence] * 100.0f),
+			   (appKitResultString != nil) ? appKitResultString : @"UNDEFINED"
 			   ];
+		
+			
+		
 		printf("%s\n\n", [str UTF8String]);
 		
 	
