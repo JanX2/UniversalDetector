f45bcd5d9b8a9bde119ed66911edac75f2fd75fd
Jan Weiß <jan@geheimwerk.de>
2014-03-04 13:19:34 +0100

    Improving error handling in DecoderTest.

--- DetectorTest.m
+++ DetectorTest.m
@@ -20,6 +20,11 @@ int main(int argc,char **argv)
 											  options:0
 												error:&error];
 		
+		if (data == nil) {
+			NSLog(@"%@", error);
+			continue;
+		}
+		
 		NSString *str = nil;
 
 		if (data == nil) {
